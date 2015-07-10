fc928b35a98b444f582844d4d166f8cb2c33a25f
jayson <chenjiazhan@hotmail.com>
2015-07-10 22:26:34 +0800

    commit

--- prometheus/src/main/java/com/web/controller/TestController.java
+++ prometheus/src/main/java/com/web/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.web.controller;
 
 import com.web.service.CrawlerService;
+import com.web.task.CrawlerTask;
+import com.web.task.DefaultCrawlerTask;
 import com.web.util.SpringFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,11 +16,19 @@ public class TestController {
     @Autowired
     private CrawlerService service;
 
-    @Autowired
     @RequestMapping("test")
 
     public String test(){
-
+        CrawlerTask task = factory.create(DefaultCrawlerTask.class);
+        task.setDepth(5)
+                .setAutoParse(true)
+                .setThreads(5)
+                .setTopN(100)
+                .setResumable(false)
+                .setCrawlPath("E:\\data")
+                .addSeed("http://blog.csdn.net")
+                .addRegex("http://.*blog.csdn.net/.*/article/details/\\d+$");
+        service.addTask(task);
         return "test";
     }
 }
